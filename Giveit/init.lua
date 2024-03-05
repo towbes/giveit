a0c046c4a38d685ce844ec5e94a18da09e8c01eb
towbes <hank.wyatt@gmail.com>
2024-03-05 11:37:41 +0900

    Refactor into separate functions
    Refactoring to separate functions in prep for trading to full raid

--- Giveit/init.lua
+++ Giveit/init.lua
@@ -1,6 +1,6 @@
 --[[
     Created by Eqplayer16
-    v.1.1
+    v.2.0
 
     Giveit is a lua script to trade items and coin with slash commands
     
@@ -9,8 +9,13 @@
         -Use quotes around the itemName if there are spaces. Quantity will default to 1 if not used, which will trade Whole stacks of stackable items
     giveit coin [pc/npc] [name] [plat/gold] [amount/all]
         -using 'all' for the amount will trade the entire amount of that coin type
+    giveit raid coin plat [amount]
+        -Gives an amount of plat to all raid members
 
-    ToDo: Implement trading multiple items at once
+    ToDo: 
+    - Implement trading multiple items at once
+    - Give every player in group/raid X item
+
 
     --Settings code from Lootly by SpecialEd https://www.redguides.com/community/resources/lootly.2196/
     --Trade code from GiveToMe by TheDroidUrLookingFor https://www.redguides.com/community/resources/give-to-me.2876/
@@ -66,6 +71,53 @@ local function Give(itemName, qty)
     end
 end
 
+local function GiveCoin(itemName, amt)
+    if itemName == 'plat' then
+        if mq.TLO.Me.Platinum() >= 1 then
+            if amt == 'all' then
+                mq.cmd('/shift /notify InventoryWindow IW_Money0 leftmouseup')
+            else
+                mq.cmd('/notify InventoryWindow IW_Money0 leftmouseup')
+                mq.delay(WaitTime)
+                mq.cmd('/notify QuantityWnd QTYW_Slider newvalue ' .. amt)
+                mq.delay(WaitTime)
+                mq.cmd('/notify QuantityWnd QTYW_Accept_Button leftmouseup')
+            end
+            mq.delay(WaitTime, CheckCursor)
+            mq.cmd('/click left target')
+            mq.delay(WaitTime, CursorEmpty)
+        end
+    elseif itemName == 'gold' then
+        if mq.TLO.Me.Gold() >= 1 then
+            if amt == 'all' then
+                mq.cmd('/shift /notify InventoryWindow IW_Money1 leftmouseup')
+            else
+                mq.cmd('/notify InventoryWindow IW_Money1 leftmouseup')
+                mq.delay(WaitTime)
+                mq.cmd('/notify QuantityWnd QTYW_Slider newvalue ' .. amt)
+                mq.delay(WaitTime)
+                mq.cmd('/notify QuantityWnd QTYW_Accept_Button leftmouseup')
+            end
+            mq.delay(WaitTime, CheckCursor)
+            mq.cmd('/click left target')
+            mq.delay(WaitTime, CursorEmpty)
+        end
+    end
+end
+
+local function OpenInventory()
+    PRINTMETHOD('Opening Inventory')
+    mq.TLO.Window('InventoryWindow').DoOpen()
+    mq.delay(1500, InventoryOpen)
+end
+
+local function ClickTrade()
+    mq.delay(WaitTime)
+    mq.cmd('/notify TradeWnd TRDW_Trade_Button leftmouseup')
+    mq.delay(WaitTime)
+end
+
+
 local function NavToTrade(navTarget)
     PRINTMETHOD('Moving to %s.', navTarget)
     mq.cmd('/nav target')
@@ -77,6 +129,17 @@ local function NavToTrade(navTarget)
     end
 end
 
+local function NavTarget(name, spawntype)
+    PRINTMETHOD('Targetting %s', name)
+    mq.cmd('/target "' .. name .. '" ' .. spawntype)
+    mq.delay(2000, HaveTarget)
+    mq.cmd('/face')
+
+    if mq.TLO.Spawn(name).Distance3D() > 20 then
+        NavToTrade(name)
+    end
+end
+
 -- script functions
 local function print_usage()
     Write.Info('\agAvailable Commands - ')
@@ -85,7 +148,7 @@ local function print_usage()
 end
 
 -- binds
-local function bind_giveit(cmd, type, name, itemName, amt)
+local function bind_giveit(cmd, val1, val2, val3, val4)
 
     -- usage
     if cmd == nil then 
@@ -94,35 +157,52 @@ local function bind_giveit(cmd, type, name, itemName, amt)
     end
 
     -- item
-    if cmd == 'item' and type ~= nil and name ~= nil and itemName ~= nil then
-        --If quantity is empty, set it to 1
-        local quantity = 1
-        if amt ~= nil then
-            quantity = tonumber(amt)
+    if cmd == 'item' then
+        local spawntype = val1
+        local name = val2
+        local itemName = val3
+        local amt = val4
+        if spawntype ~= nil and name ~= nil and itemName ~= nil then
+            --If quantity is empty, set it to 1
+            local quantity = 1
+            if amt ~= nil then
+                quantity = tonumber(amt)
+            end
+            OpenInventory()
+            NavTarget(name, spawntype)
+
+            Give(itemName, quantity)
+
+            ClickTrade()
+
+        else
+            print_usage() 
         end
-		PRINTMETHOD('Opening Inventory')
-		mq.TLO.Window('InventoryWindow').DoOpen()
-		mq.delay(1500, InventoryOpen)
-
-		PRINTMETHOD('Targetting %s', name)
-		mq.cmd('/target "' .. name .. '" ' .. type)
-		mq.delay(2000, HaveTarget)
-		mq.cmd('/face')
-
-		if mq.TLO.Spawn(name).Distance3D() > 20 then
-			NavToTrade(name)
-		end
-
-		Give(itemName, quantity)
-
-		mq.delay(WaitTime)
-		mq.cmd('/notify TradeWnd TRDW_Trade_Button leftmouseup')
-		mq.delay(WaitTime)
 
     end
 
     -- item
-    if cmd == 'coin' and type ~= nil and name ~= nil and itemName ~= nil and amt ~= nil then
+    if cmd == 'coin' then
+        local spawntype = val1
+        local name = val2
+        local itemName = val3
+        local amt = val4
+        if spawntype ~= nil and name ~= nil and itemName ~= nil and amt ~= nil then
+            OpenInventory()
+
+            NavTarget(name, spawntype)
+
+            GiveCoin(itemName, amt)
+
+            ClickTrade()
+        else
+            print_usage() 
+        end
+
+    end
+
+    -- raid
+    if cmd == 'raid' and type ~= nil and name ~= nil and itemName ~= nil and amt ~= nil then
 		PRINTMETHOD('Opening Inventory')
 		mq.TLO.Window('InventoryWindow').DoOpen()
 		mq.delay(1500, InventoryOpen)
